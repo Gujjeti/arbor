d03a3a5252e764ea60a68c79a9e2e2d6259dec4a
Ganesh Gujjeti (Omnicom Production) <ganesh.gujjeti@omc.com>
2025-08-26 19:31:04 +0530

    changes

--- public/js/main.js
+++ public/js/main.js
@@ -565,7 +565,14 @@ $(document).ready(function () {
 }
 
 
-
+$(".dropdown").hover(
+    function () {
+      $(this).find(".dropdown-menu").stop(true, true).slideDown(200);
+    },
+    function () {
+      $(this).find(".dropdown-menu").stop(true, true).slideUp(200);
+    }
+  );
 
 
  
